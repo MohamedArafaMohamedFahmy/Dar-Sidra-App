dfed77c2bbdc97338b812b9306d3d67ba70a5b64
Mohamed Arafa <mohammedarafa011@gmail.com>
2021-10-26 22:41:05 +0200

    Dar Sidra App

--- app/src/main/java/com/arafa/mohamed/darsidraapp/activities/LoginActivity.java
+++ app/src/main/java/com/arafa/mohamed/darsidraapp/activities/LoginActivity.java
@@ -1,30 +1,47 @@
 package com.arafa.mohamed.darsidraapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 
 import com.arafa.mohamed.darsidraapp.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Locale;
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+    TextInputEditText etPassword;
+    TextInputLayout textLayoutPassword;
+    boolean checkPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setLocale();
         setContentView(R.layout.activity_login);
-    }
 
-    public void setLocale() {
-        Locale locale = new Locale("ar");
-        Locale.setDefault(locale);
-        Resources resources = getResources();
-        Configuration config = resources.getConfiguration();
-        config.setLocale(locale);
-        resources.updateConfiguration(config, resources.getDisplayMetrics());
+        etPassword = findViewById(R.id.editText_password);
+        textLayoutPassword = findViewById(R.id.password_layout);
+        etPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        checkPassword = true;
+
+        textLayoutPassword.setStartIconOnClickListener(v -> {
+            if(Objects.requireNonNull(AppCompatResources.getDrawable(LoginActivity.this, R.drawable.ic_eye)).isVisible() && checkPassword ){
+                textLayoutPassword.setStartIconDrawable(R.drawable.ic_eye_off);
+                etPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                checkPassword = false;
+            }
+            else {
+                textLayoutPassword.setStartIconDrawable(R.drawable.ic_eye);
+                etPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                checkPassword = true;
+            }
+        });
     }
 }
--- app/src/main/java/com/arafa/mohamed/darsidraapp/activities/RegistrationActivity.java
+++ app/src/main/java/com/arafa/mohamed/darsidraapp/activities/RegistrationActivity.java
@@ -1,16 +1,72 @@
 package com.arafa.mohamed.darsidraapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 import com.arafa.mohamed.darsidraapp.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Locale;
+import java.util.Objects;
 
 public class RegistrationActivity extends AppCompatActivity {
+    TextInputEditText etPassword,etConformPassword;
+    TextInputLayout textLayoutPassword,textLayoutConfirmPassword;
+    boolean checkPassword,checkConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        etPassword = findViewById(R.id.editText_password);
+        etConformPassword = findViewById(R.id.editText_confirm_password);
+        textLayoutPassword = findViewById(R.id.password_layout);
+        textLayoutConfirmPassword = findViewById(R.id.confirm_password_layout);
+        etPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        etConformPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        checkPassword = true;
+        checkConfirmPassword = true;
+
+        textLayoutPassword.setStartIconOnClickListener(v -> {
+            if(Objects.requireNonNull(AppCompatResources.getDrawable(RegistrationActivity.this, R.drawable.ic_eye)).isVisible() && checkPassword ){
+                textLayoutPassword.setStartIconDrawable(R.drawable.ic_eye_off);
+                etPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                checkPassword = false;
+            }
+            else {
+                textLayoutPassword.setStartIconDrawable(R.drawable.ic_eye);
+                etPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                checkPassword = true;
+            }
+        });
+
+
+        textLayoutConfirmPassword.setStartIconOnClickListener(v -> {
+            if(Objects.requireNonNull(AppCompatResources.getDrawable(RegistrationActivity.this, R.drawable.ic_eye)).isVisible() && checkConfirmPassword ){
+                textLayoutConfirmPassword.setStartIconDrawable(R.drawable.ic_eye_off);
+                etConformPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                checkConfirmPassword = false;
+            }
+            else {
+                textLayoutConfirmPassword.setStartIconDrawable(R.drawable.ic_eye);
+                etConformPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                checkConfirmPassword = true;
+            }
+        });
+
     }
+
 }
